580ac8f732fca66f117770d09a67afd759660d49
jeremie-abt <abtjeremie@gmail.com>
2021-11-10 20:55:58 +0100

    separate tree from node

--- tree.go
+++ tree.go
@@ -1,10 +1,72 @@
 package second_tree
 
-type Tree struct {}
+import "errors"
 
-func NewTree(values ...interface{}) (Tree, error) {
-	return &tree{}, nil
+type node struct {
+	parent *node // -> 4
+	left   *node // -> 3
+	right  *node // -> 5
+
+	value interface{}
 }
 
+/*
+Private interface
+ */
 
+func newNode(val interface{}) (*node, error) {
 
+	// TODO
+	return &node{
+		value: val,
+	}, nil
+}
+
+func (node *node) addLeftChild (val interface{}) error {
+	var err error = nil
+	if node.left != nil {
+		return errors.New("this node has already left child")
+	}
+	node.left, err = NewNode(val)
+	return err
+}
+
+func (node *node) addRightChild (val interface{}) (error) {
+	node.right.value = val
+	return nil
+}
+
+/*
+Public Interface
+ */
+
+type Tree struct {
+	rootNode *node
+}
+
+func NewTree(val ...interface{}) (*Tree, error) {
+
+	var err error = nil
+
+	if len(val) != 1 {
+		panic("not implemented yet")
+	}
+	node, err := newNode(val[0])
+	return &Tree{
+		rootNode: node,
+	}, err
+}
+
+func (node *Tree) Insert(vals ...interface{}) error {
+	return nil
+}
+
+func (node *Tree) Delete(vals ...interface{}) error {
+	return nil
+}
+
+func (node *Tree) GetAsList() []interface{} {
+	return nil
+}
+
+func (node *Tree) AddTriFunc(func(interface{}, interface{}) int) {}
--- tree_test.go
+++ tree_test.go
@@ -1,20 +1,62 @@
 package second_tree
 
 import (
-	"fmt"
+	"gotest.tools/assert/cmp"
 	"testing"
 )
 import "reflect"
 
 import "gotest.tools/assert"
 
+/*
+Private test for tree
+
+func addLeftChild()  // work only on leaf who has the place to have left child
+func addRightChild() // work only on leaf who has the place to have left child
+
+func removeNode() // work on the specific node
+*/
+
+func TestAddLeftChild(t *testing.T) {
+	node, _ := NewNode(15)
+	err := node.addLeftChild(9)
+	assert.NilError(t, err)
+	err = node.left.addLeftChild(10)
+	assert.NilError(t, err)
+	err = node.addLeftChild(11)
+	assert.Error(t, err, "this node has already left child")
+
+	assert.Assert(t, cmp.Nil(node.right))
+	assert.Assert(t, node.value.(int) == 15)
+	//assert.Assert(t, node.left.value.(int) == 9)
+	//assert.Assert(t, node.left.left.value.(int) == 10)
+}
+
+func TestAddRightChild(t *testing.T) {
+	node, _ := NewNode(15)
+	err := node.addRightChild(9)
+	assert.NilError(t, err)
+	err = node.right.addRightChild(10)
+	assert.NilError(t, err)
+	err = node.addRightChild(11)
+	assert.Error(t, err, "this node has already right child")
+
+	assert.Assert(t, cmp.Nil(node.right))
+	assert.Assert(t, node.value.(int) == 15)
+	assert.Assert(t, node.right.value.(int) == 9)
+	assert.Assert(t, node.right.right.value.(int) == 10)
+}
+
+/*
+Test for public interface
+*/
 
 func TestNewTreeInterface(t *testing.T) {
-	firstTree, err1 := NewTree("test", "test2", "test3")
-	secTree, err2 := NewTree([]string{"test", "test2", "test3"})
-	thirdTree, err3 := NewTree("test", "test2")
-	fourthTree, err4 := NewTree(6, 13, 15, 4, 2)
-	fifthTree, err5 := NewTree(6, 15, 13, 2, 4)
+	firstTree, err1 := NewNode("test", "test2", "test3")
+	secTree, err2 := NewNode([]string{"test", "test2", "test3"})
+	thirdTree, err3 := NewNode("test", "test2")
+	fourthTree, err4 := NewNode(6, 13, 15, 4, 2)
+	fifthTree, err5 := NewNode(6, 15, 13, 2, 4)
 
 	assert.DeepEqual(t, firstTree, secTree)
 	assert.Check(t, !reflect.DeepEqual(firstTree, thirdTree))
@@ -26,32 +68,34 @@ func TestNewTreeInterface(t *testing.T) {
 	assert.NilError(t, err5)
 }
 
-
 // linked to TestEnforceTyping function.
 type firstCustomType struct {
 	value int
 }
+
 // linked to TestEnforceTyping function.
 type secondCustomType struct {
 	value int
 }
+
 // linked to TestEnforceTyping function.
 type interfaceTest interface{}
+
 func TestEnforceTyping(t *testing.T) {
 	// Values for a binary tree can be of different types.
 	// This test verify that statement hold true.
 
-	_, err := NewTree("test", "test2", 5)
+	_, err := NewNode("test", "test2", 5)
 	assert.Error(t, err, "tree can't hold different type of data")
 
-	_, err = NewTree(interfaceTest(firstCustomType{value: 5}), interfaceTest(secondCustomType{value: 5}))
+	_, err = NewNode(interfaceTest(firstCustomType{value: 5}), interfaceTest(secondCustomType{value: 5}))
 	assert.Check(t, err, "tree can't hold different type of data")
 
 	// tree must accept interface values with same concrete type
-	_, err = NewTree(interfaceTest(firstCustomType{value: 6}), interfaceTest(firstCustomType{value: 5}))
+	_, err = NewNode(interfaceTest(firstCustomType{value: 6}), interfaceTest(firstCustomType{value: 5}))
 	assert.NilError(t, err)
 
-	testedTree, _ := NewTree()
+	testedTree, _ := NewNode()
 	err = testedTree.Insert(5, 4)
 	assert.NilError(t, err)
 	err = testedTree.Insert("bad string data type")
@@ -94,7 +138,7 @@ func TestDelete(t *testing.T) {
 }
 
 func TestGetAsList(t *testing.T) {
-	testedTree, _ := NewTree(8,5,6,9,78,45,6)
+	testedTree, _ := NewTree(8, 5, 6, 9, 78, 45, 6)
 
 	assert.DeepEqual(t, []int{5, 6, 6, 8, 9, 45, 78}, testedTree.GetAsList())
 	testedTree.AddTriFunc(func(first interface{}, sec interface{}) int {
